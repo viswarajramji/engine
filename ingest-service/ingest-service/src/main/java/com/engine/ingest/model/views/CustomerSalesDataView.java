d6fc65aa26b5b9390940bc9dac1895b980e35997
Viswarajramji <viswarajramji93@gmail.com>
2024-08-06 11:28:27 +0400

    Complete

--- ingest-service/ingest-service/src/main/java/com/engine/ingest/model/views/CustomerSalesDataView.java
+++ ingest-service/ingest-service/src/main/java/com/engine/ingest/model/views/CustomerSalesDataView.java
@@ -11,130 +11,137 @@ import java.time.LocalDate;
 @Table(name = "CUSTOMER_DATA_VIEW") // Use the view name as the table name
 public class CustomerSalesDataView {
 
-        @Id
-        @Column(name = "TRXN_Timestamp")
-        @JsonProperty("TRXN_Timestamp")
-        private String transactionTimestamp;
+    @Id
+    @Column(name = "TRXN_TIMESTAMP")
+    @JsonProperty("TRXN_Timestamp")
+    private String trxnTimestamp;
 
-        @Column(name = "Car_Driving_Status")
-        @JsonProperty("Car_Driving_Status")
-        private String carDrivingStatus;
+    @Column(name = "CAR_DRIVING_STATUS")
+    @JsonProperty("Car_Driving_Status")
+    private String carDrivingStatus;
 
-        @Column(name = "Current_Longitude")
-        @JsonProperty("Current_Longitude")
-        private Double currentLongitude;
+    @Column(name = "CURRENT_LONGITUDE")
+    @JsonProperty("Current_Longitude")
+    private Double currentLongitude;
 
-        @Column(name = "Current_Latitude")
-        @JsonProperty("Current_Latitude")
-        private Double currentLatitude;
+    @Column(name = "CURRENT_LATITUDE")
+    @JsonProperty("Current_Latitude")
+    private Double currentLatitude;
 
-        @Column(name = "Current_Area")
-        @JsonProperty("Current_Area")
-        private String currentArea;
+    @Column(name = "CURRENT_AREA")
+    @JsonProperty("Current_Area")
+    private String currentArea;
 
-        @Column(name = "KM")
-        @JsonProperty("KM")
-        private Double km;
+    @Column(name = "KM")
+    @JsonProperty("KM")
+    private Double km;
 
-        @Column(name = "Amount")
-        @JsonProperty("Amount")
-        private Double amount;
+    @Column(name = "AMOUNT")
+    @JsonProperty("Amount")
+    private Double amount;
 
-        @Column(name = "customer_mobile_no")
-        @JsonProperty("customer_mobile_no")
-        private String customerMobileNo;
+    @Column(name = "customer_mobile_no")
+    @JsonProperty("customer_mobile_no")
+    private String customerMobileNo;
 
-        @Column(name = "Customer_Name")
-        @JsonProperty("Customer_Name")
-        private String customerName;
+    @Column(name = "Customer_Name")
+    @JsonProperty("Customer_Name")
+    private String customerName;
 
-        @Column(name = "Customer_Gender")
-        @JsonProperty("Customer_Gender")
-        private String customerGender;
+    @Column(name = "Customer_Gender")
+    @JsonProperty("Customer_Gender")
+    private String customerGender;
 
-        @Column(name = "Customer_Age")
-        @JsonProperty("Customer_Age")
-        private Integer customerAge;
+    @Column(name = "Customer_Age")
+    @JsonProperty("Customer_Age")
+    private Integer customerAge;
 
-        @Column(name = "Customer_Nationality")
-        @JsonProperty("Customer_Nationality")
-        private String customerNationality;
+    @Column(name = "Customer_Age_Band")
+    @JsonProperty("Customer_Age_Band")
+    private String customerAgeBand;
 
-        @Column(name = "Customer_PassportNo")
-        @JsonProperty("Customer_PassportNo")
-        private String customerPassportNo;
+    @Column(name = "Customer_Nationality")
+    @JsonProperty("Customer_Nationality")
+    private String customerNationality;
 
-        @Column(name = "Customer_Id_No")
-        @JsonProperty("Customer_Id_No")
-        private String customerIdNo;
+    @Column(name = "Customer_Passport_No")
+    @JsonProperty("Customer_Passport_No")
+    private String customerPassportNo;
 
-        @Column(name = "Customer_Home_address")
-        @JsonProperty("Customer_Home_address")
-        private String customerHomeAddress;
+    @Column(name = "Customer_Id_No")
+    @JsonProperty("Customer_Id_No")
+    private String customerIdNo;
 
-        @Column(name = "Customer_Lease_Start_Date")
-        @JsonProperty("Customer_Lease_Start_Date")
-        private String customerLeaseStartDate;
+    @Column(name = "Customer_Home_address")
+    @JsonProperty("Customer_Home_address")
+    private String customerHomeAddress;
 
-        @Column(name = "Customer_Lease_Period")
-        @JsonProperty("Customer_Lease_Period")
-        private Integer customerLeasePeriod;
+    @Column(name = "Customer_Lease_Start_Date")
+    @JsonProperty("Customer_Lease_Start_Date")
+    private String customerLeaseStartDate;
 
-        @Column(name = "Car_Make")
-        @JsonProperty("Car_Make")
-        private String carMake;
+    @Column(name = "Customer_Lease_Period")
+    @JsonProperty("Customer_Lease_Period")
+    private Integer customerLeasePeriod;
 
-        @Column(name = "Car_Model")
-        @JsonProperty("Car_Model")
-        private String carModel;
+    @Column(name = "Car_Make")
+    @JsonProperty("Car_Make")
+    private String carMake;
 
-        @Column(name = "Car_Plate_No")
-        @JsonProperty("Car_Plate_No")
-        private String carPlateNo;
+    @Column(name = "Car_Model")
+    @JsonProperty("Car_Model")
+    private String carModel;
 
-        @Column(name = "Car_Registration_Date")
-        @JsonProperty("Car_Registration_Date")
-        private String carRegistrationDate;
+    @Column(name = "Car_Plate_No")
+    @JsonProperty("Car_Plate_No")
+    private String carPlateNo;
 
-        @Column(name = "Car_Registration_Expiry_Date")
-        @JsonProperty("Car_Registration_Expiry_Date")
-        private String carRegistrationExpiryDate;
+    @Column(name = "Car_Registration_Date")
+    @JsonProperty("Car_Registration_Date")
+    private String carRegistrationDate;
 
-        @Column(name = "Office_Mobile_No")
-        @JsonProperty("Office_Mobile_No")
-        private String officeMobileNo;
+    @Column(name = "Car_Registration_Expiry_Date")
+    @JsonProperty("Car_Registration_Expiry_Date")
+    private String carRegistrationExpiryDate;
 
-        @Column(name = "Office_Area")
-        @JsonProperty("Office_Area")
-        private String officeArea;
+    @Column(name = "Office_Mobile_No")
+    @JsonProperty("Office_Mobile_No")
+    private String officeMobileNo;
 
-        @Column(name = "Office_Office_No")
-        @JsonProperty("Office_Office_No")
-        private String officeOfficeNo;
+    @Column(name = "Office_Area")
+    @JsonProperty("Office_Area")
+    private String officeArea;
 
-        @Column(name = "Office_Working_Hours")
-        @JsonProperty("Office_Working_Hours")
-        private String officeWorkingHours;
+    @Column(name = "Office_Office_No")
+    @JsonProperty("Office_Office_No")
+    private String officeOfficeNo;
 
-        @Column(name = "Agent_Mobile_No")
-        @JsonProperty("Agent_Mobile_No")
-        private String agentMobileNo;
+    @Column(name = "Office_Working_Hours")
+    @JsonProperty("Office_Working_Hours")
+    private String officeWorkingHours;
 
-        @Column(name = "Agent_Name")
-        @JsonProperty("Agent_Name")
-        private String agentName;
+    @Column(name = "Agent_Mobile_No")
+    @JsonProperty("Agent_Mobile_No")
+    private String agentMobileNo;
 
-        @Column(name = "Agent_Gender")
-        @JsonProperty("Agent_Gender")
-        private String agentGender;
+    @Column(name = "Agent_Name")
+    @JsonProperty("Agent_Name")
+    private String agentName;
 
-        @Column(name = "Agent_Age")
-        @JsonProperty("Agent_Age")
-        private Integer agentAge;
+    @Column(name = "Agent_Gender")
+    @JsonProperty("Agent_Gender")
+    private String agentGender;
 
-        @Column(name = "Agent_Nationality")
-        @JsonProperty("Agent_Nationality")
-        private String agentNationality;
+    @Column(name = "Agent_Age")
+    @JsonProperty("Agent_Age")
+    private Integer agentAge;
 
+    @Column(name = "Agent_Age_Band")
+    @JsonProperty("Agent_Age_Band")
+    private String agentAgeBand;
+
+    @Column(name = "Agent_Nationality")
+    @JsonProperty("Agent_Nationality")
+    private String agentNationality;
     // Getters and Setters
 }
--- ingest-service/ingest-service/src/main/resources/schema.sql
+++ ingest-service/ingest-service/src/main/resources/schema.sql
@@ -1,66 +1,61 @@
-CREATE VIEW CUSTOMER_DATA_VIEW AS
-SELECT
+create view CUSTOMER_DATA_VIEW as 
+
+SELECT 
     gps.TRXN_TIMESTAMP AS TRXN_Timestamp,
     gps.CAR_DRIVING_STATUS AS Car_Driving_Status,
     gps.CURRENT_LONGITUDE AS Current_Longitude,
     gps.CURRENT_LATITUDE AS Current_Latitude,
     gps.CURRENT_AREA AS Current_Area,
     gps.KM AS KM,
-
     st.AMOUNT AS Amount,
-
     cd.MOBILE_NO AS customer_mobile_no,
     cd.NAME AS Customer_Name,
     cd.GENDER AS Customer_Gender,
     cd.AGE AS Customer_Age,
-    CASE
+    CASE 
         WHEN cd.AGE < 18 THEN '<18'
         WHEN cd.AGE BETWEEN 19 AND 30 THEN '19-30'
         WHEN cd.AGE BETWEEN 31 AND 50 THEN '31-50'
         ELSE '51+'
     END AS Customer_Age_Band,
     cd.NATIONALITY AS Customer_Nationality,
-    cd.PASSPORT_NO AS Customer_PassportNo,
+    cd.PASSPORT_NO AS Customer_Passport_No,
     cd.ID_NO AS Customer_Id_No,
     cd.HOME_ADDRESS AS Customer_Home_address,
     cd.LEASE_START_DATE AS Customer_Lease_Start_Date,
     cd.LEASE_PERIOD AS Customer_Lease_Period,
-
     car.CAR_MAKE AS Car_Make,
     car.CAR_MODEL AS Car_Model,
     car.PLATE_NO AS Car_Plate_No,
     car.REGISTRATION_DATE AS Car_Registration_Date,
     car.REGISTRATION_EXPIRY_DATE AS Car_Registration_Expiry_Date,
-
     obd.MOBILE_NO AS Office_Mobile_No,
     obd.AREA AS Office_Area,
     obd.OFFICE_NO AS Office_Office_No,
     obd.WORKING_HOURS AS Office_Working_Hours,
-
     sgd.MOBILE_NO AS Agent_Mobile_No,
     sgd.NAME AS Agent_Name,
     sgd.GENDER AS Agent_Gender,
     sgd.AGE AS Agent_Age,
-    CASE
+    CASE 
         WHEN sgd.AGE < 18 THEN '<18'
         WHEN sgd.AGE BETWEEN 19 AND 30 THEN '19-30'
         WHEN sgd.AGE BETWEEN 31 AND 50 THEN '31-50'
         ELSE '51+'
     END AS Agent_Age_Band,
     sgd.NATIONALITY AS Agent_Nationality
-
-FROM
+FROM 
     GPS_TRANSACTIONS gps
-JOIN
+JOIN 
     CAR_DATA car ON car.CAR_ID = gps.CAR_ID
-JOIN
+JOIN 
     SALES_AGENT_DATA sgd ON sgd.AGENT_ID = gps.AGENT_ID
-JOIN
+JOIN 
     CUSTOMER_DATA cd ON cd.CUSTOMER_ID = gps.CUSTOMER_ID
-JOIN
+JOIN 
     OFFICE_BRANCH_DATA obd ON obd.OFFICE_ID = gps.OFFICE_ID
-JOIN
-    SALES_TRANSACTIONS st ON st.CUSTOMER_ID = gps.CUSTOMER_ID
-    AND st.CAR_ID = gps.CAR_ID
-    AND st.OFFICE_ID = gps.OFFICE_ID
+JOIN 
+    SALES_TRANSACTIONS st ON st.CUSTOMER_ID = gps.CUSTOMER_ID 
+    AND st.CAR_ID = gps.CAR_ID 
+    AND st.OFFICE_ID = gps.OFFICE_ID 
     AND st.AGENT_ID = gps.AGENT_ID;
